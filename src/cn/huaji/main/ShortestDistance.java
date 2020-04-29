5d5b4e7977f0e3fcd46527537057002c61e463e5
GitHub121380 <903329290@qq.com>
2020-04-29 17:18:41 +0800

    2020/4/29 更新Dijkstra算法

--- src/cn/huaji/main/ShortestDistance.java
+++ src/cn/huaji/main/ShortestDistance.java
@@ -1,112 +1,111 @@
 package cn.huaji.main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
 /**
  * @ClassName ShortestDistance
- * @Description 给定平面上N个点的坐标,找出距离最近的两个点
+ * @Description 最短路径问题
  * @author 滑技工厂 https://blog.csdn.net/qq_41718454
- * @date 2020/3/28
+ * @date 2020/4/29
  * @version 1.0
  */
+
 public class ShortestDistance {
-
-    public static void main(String[] args) {
-        // 测试用例
-        Point[] points = new Point[7];
-
-        points[0] = new Point(1, 1);
-        points[1] = new Point(1, 9);
-        points[2] = new Point(2, 5);
-        points[3] = new Point(3, 1);
-        points[4] = new Point(4, 4);
-        points[5] = new Point(5, 8);
-        points[6] = new Point(6, 2);
-
-        // 预处理，基于x轴坐标排序，便于分治法实施
-        Arrays.sort(points, new Comparator<Point>() {
-            @Override
-            public int compare(Point p1, Point p2) {
-                return (p1.x > p2.x) ? 1 : (p1.x == p2.x) ? 0 : -1;
-            }
-
-        });
-        // 测试
-        System.out.println(divide(0, points.length - 1, points));
-    }
+    public static final int MAX = 1000000000;
 
     /*
-     * @Title divide
-     * @Description 求平面上距离最近的两个点
+     * @Title dijkstra
+     * @Description dist[j]=min{dist[j],dist[i]+matrix[i][j]}
      * @author 滑技工厂
-     * @Date 2020/3/28
-     * @param [left, right, points]
-     * @return double
+     * @Date 2020/4/29
+     * @param [matrix]
+     * @return int[] 最短距离数组
      * @throws
      */
-    public static double divide(int left, int right, Point[] points) {
-        // 当前最小两点距离，初始值设置为无穷大
-        double curMinDis = 1e20;
-        // 如果只有一个点，则不存在最近两点距离，返回无穷大
-        if (left == right) {
-            return curMinDis;
+    public static int[] dijkstra(int[][] matrix) {
+        int[] distance = new int[matrix.length];
+        //最短距离数组初始化 除了源点都默认到v0与各个结点的距离（不可达的是MAX）
+        for (int i = 1; i < distance.length; i++) {
+            distance[i] = matrix[0][i];
         }
-        // 这里是判断是否为只有两个点，如果只有两个点的话那么直接求解。
-        if (left + 1 == right) {
-            return distance(points[left], points[right]);
+        boolean[] visit = new boolean[distance.length];
+        //到源点到每个点的最短距离的路径
+        String[] path = new String[distance.length];
+        for (int i = 0; i < path.length; i++) {
+            //初始化默认从源点直达，后面如果途径中间结点会修改
+            path[i] = "0-->" + i;
         }
-
-        // 分治法：第一步：分区，并求取左右分区最小两点距离
-        // 通过右移运算除2，对区域进行合理的划分，使得左右两边保持大致相等个数点
-        int middle = (left + right) >> 1;
-        double leftMinDis = divide(left, middle, points);
-        double rightMinDis = divide(middle, right, points);
-
-        curMinDis = (leftMinDis <= rightMinDis) ? leftMinDis : leftMinDis;
-
-        // 分治法：第二步：假设距离最近的两点分别在左右分区中
-        // 关键代码，距离最近的两个点，一个位于左边区域，一个位于右边区域，x轴搜索范围[middle-curMinDis, middle+curMinDis]
-        // 记录搜索区间内的点的索引，便于进一步计算最小距离
-        List<Integer> validPointIndex = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            if (Math.abs(points[middle].x - points[i].x) <= curMinDis) {
-                validPointIndex.add(i);
-            }
-        }
-        // 基于索引，进一步计算区间内最小两点距离
-        for (int i = 0; i < validPointIndex.size() - 1; i++) {
-            for (int j = i + 1; j < validPointIndex.size(); j++) {
-                // 如果区间内的两点y轴距离大于curMinDis，则没必要计算了，因为，它们的距离肯定大于curMinDis，
-                if (Math.abs(points[validPointIndex.get(i)].y
-                        - points[validPointIndex.get(j)].y) > curMinDis) {
-                    continue;
+        //已经访问过源点
+        visit[0] = true;
+        //表示计数，已经访问过一个结点
+        int count = 1;
+        //当count==结点数时结束循环
+        while (count++ < distance.length) {
+            //从刚被访问的结点可直达的结点中找一个distance最小的结点返回下标，即为下一个要访问的
+            //但是这个方法其实是遍历所有未被访问的结点，因为访问不到的为MAX所以意义一样
+            int index = min(distance, visit);
+            visit[index] = true;
+            //添加过新访问的结点后，重新计算到各点最短路径，如果某结点有着能通过新访问结点的最短路径（比原来大），那么将其最短路径值更新
+            for (int j = 1; j < distance.length; j++) {
+                if (!visit[j] && distance[j] > distance[index] + matrix[index][j]) {
+                    distance[j] = distance[index] + matrix[index][j];
+                    path[j] = path[index] + "-->" + j;
                 }
-                double tempDis = distance(points[validPointIndex.get(i)],
-                        points[validPointIndex.get(j)]);
 
-                curMinDis = (tempDis < curMinDis) ? tempDis : curMinDis;
             }
+
         }
 
-        return curMinDis;
+        for (int i = 0; i < path.length; i++) {
+            System.out.println(path[i]);
+        }
+        return distance;
     }
 
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+
+    /*
+     * @Title min
+     * @Description 返回距离数组中未确定元素的下标
+     * @author 滑技工厂
+     * @Date 2020/4/29
+     * @param [distance当前的最短距离数组（包含已确定和未确定和正在确定）, visit表示结点是否为确定状态]
+     * @return int
+     * @throws
+     */
+    public static int min(int[] distance,boolean[] visit) {
+        int min = MAX;
+        int index = 0;
+        //省略源点v0的距离（因为值为0），从点v1开始
+        for (int i = 1; i < distance.length; i++) {
+            //说明是未确定过最短路径的点
+            if (!visit[i]&&distance[i] < min) {
+                min = distance[i];
+                index = i;
+            }
+
+        }
+        return index;
     }
-}
 
+    public static void main(String[] args) {
+//        int[][] matrix = new int[6][6];
+//        //矩阵初始化 所有距离都先默认到最大值
+//        for (int i = 0; i < matrix.length; i++) {
+//
+//            for (int j = 0; j < matrix[1].length; j++) {
+//                matrix[i][j] = MAX;
+//            }
+//        }
+        int[][] weight = {//邻接矩阵
+                {0, 3, 20, 7, MAX},
+                {3, 0, 4, 2, MAX},
+                {MAX, 4, 0, 5, 4},
+                {7, 2, 5, 0, 6},
+                {MAX, MAX, 4, 6, 0}
+        };
+        int[] distance = dijkstra(weight);
 
-class Point {
-    public int x;
-    public int y;
-
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        for (int i = 0; i < distance.length; i++) {
+            System.out.print(distance[i]+" ");
+        }
     }
 }
